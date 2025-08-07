68f05de032bbc565d56e7e9612ecb513c5d2fc82
SpyDev14 <vlad.buklovsky@gmail.com>
2025-08-07 15:00:18 +0400

    remove comment field from FeedbackRequest model

--- requests/models.py
+++ requests/models.py
@@ -10,7 +10,6 @@ from shared.string_processing.resizing import truncate_string
 class FeedbackRequest(models.Model):
 	requestener_name = models.CharField(max_length = 24, verbose_name = "Имя")
 	phone_number     = PhoneNumberField(verbose_name = "Номер телефона")
-	comment          = models.TextField(blank = True, max_length = 2048, verbose_name = "Комментарий")
 	created_at       = models.DateTimeField(auto_now_add = True, verbose_name = "Дата заполнения заявки")
 
 	class Meta:
@@ -18,7 +17,4 @@ class FeedbackRequest(models.Model):
 		verbose_name_plural = 'Заявки на обратную связь'
 
 	def __str__(self):
-		return (
-			f'Заявка от {self.requestener_name} {self.phone_number}' +
-			(f': {truncate_string(self.comment, 24)}' if self.comment else '')
-		)
+		return f'Заявка от {self.requestener_name} {self.phone_number}'
