52cefb94e7d4aa96fb843d1a666d2e83692c00e7
SpyDev14 <vlad.buklovsky@gmail.com>
2025-09-03 12:28:25 +0400

    Improve BaseRenderableModelAdmin

--- core/admin/bases.py
+++ core/admin/bases.py
@@ -1,5 +1,18 @@
-from django.contrib import admin
+from django.utils.safestring 	import mark_safe
+from django.core.exceptions 	import ImproperlyConfigured
+from django.contrib 			import admin
+
+from core.models.bases import BaseRenderableModel
 
 
 class BaseRenderableModelAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'slug': ['name']}
+	list_display = ('__str__', 'absolute_url')
+
+	def absolute_url(self, obj: BaseRenderableModel):
+		try:
+			link = obj.get_absolute_url()
+			return mark_safe(f'<a href="{link}">{link}</a>')
+		except ImproperlyConfigured:
+			return mark_safe('<span style="color: red;">Не настроено</span>')
+	absolute_url.short_description = 'URL Путь'
