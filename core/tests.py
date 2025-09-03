6b001583cfbc098fa4378001a163614c916eae20
SpyDev14 <vlad.buklovsky@gmail.com>
2025-09-03 13:15:00 +0400

    Add comment in core/tests.py

--- core/tests.py
+++ core/tests.py
@@ -4,9 +4,13 @@ from core.models.general 	import TelegramSendingChannel
 
 # Я не буду писать тесты, но я хотел проверить именно это взаимодействие
 # и вместо проверки в shell решил сделать вот так
+
+# Тут я проверяю, что не будет ошибки если специализации нет в бд
 class TelegramSendingChannelTest(TestCase):
 	def test_get_by_specialization(self):
 		specialization = TelegramSendingChannel.Specialization.LOGS
 		channel = TelegramSendingChannel.get_by_specialization(specialization)
 
 		self.assertIsNone(channel)
+
+# Делал бы тесты - проверил бы, что оно не ходит каждый раз в БД
