4ab7a55bce752ffc37193362679ffe59fc652ff6
SpyDev14 <vlad.buklovsky@gmail.com>
2025-09-25 11:30:13 +0400

    add dump_to_json template filter tag

--- core/templatetags/project_tags.py
+++ core/templatetags/project_tags.py
@@ -1,4 +1,6 @@
 # Эти теги по умолчанию добавляются в каждый темплейт (указано в настройках)
+from typing import Any
+import json
 
 from django.utils.safestring 	import mark_safe
 from django.template 			import Template, RequestContext, Library
@@ -23,6 +25,10 @@ def render_as_template(context: RequestContext, template_string: str):
 def get_attr(obj, attr_name: str, default = None):
 	print(getattr(obj, attr_name, default))
 	return getattr(obj, attr_name, default)
+
+@register.filter
+def dump_to_json(obj: Any, indent: int | None = None):
+	return json.dumps(obj, indent=indent, ensure_ascii=False)
 
 @register.inclusion_tag('core/generic/components/item_card.html')
 def item_card(title, desc = None, image = None, *, url):
