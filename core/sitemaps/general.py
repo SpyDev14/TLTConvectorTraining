dcbf790645018c191b5ee897c43ad705651adc85
SpyDev14 <vlad.buklovsky@gmail.com>
2025-09-26 13:04:17 +0400

    remove DebugSitemap (i test all; i didn't want add him in history)

--- core/sitemaps/general.py
+++ core/sitemaps/general.py
@@ -1,13 +1,6 @@
-from django.contrib.sitemaps 	import Sitemap, GenericSitemap
-from django.urls 				import reverse
+from django.contrib.sitemaps 	import GenericSitemap
 
-from core.models.general 	import Page
 from core.models.bases 		import BaseRenderableModel
 from shared.reflection 		import get_subclasses
 
-class DebugSitemap(Sitemap):
-	def items(self):
-		return Page.objects.all()
 
-	def location(self, obj: Page):
-		return reverse('debug-repr', args=[obj.slug])
