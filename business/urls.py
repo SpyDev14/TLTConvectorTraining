8d29d2caf6c99cc6b74ce1ffdcd0edbd3422ad1d
SpyDev14 <vlad.buklovsky@gmail.com>
2025-09-29 08:59:03 +0400

    add comment for front's about data sources in templates in business.urls (view names only for back's, use other sources instead)

--- business/urls.py
+++ business/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 
 from business import views
 
-
+# INFO: view names - только для работы ИЗ КОДА!!!
+# В темплейтах используйте другие источники: get_absolute_url() от объекта и global объекты
+# {% for item in items %}
+# 	{{ item.get_absolute_url }}
+# {% endfor %}
+# {{ global.pages.contacts.get_absolute_url }}
 urlpatterns = [
 	path('', views.HomePageView.as_view(), name = 'index'),
 	path('about-us/', views.AboutUsPageView.as_view(), name = 'about-us'),
