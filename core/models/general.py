cb535e17686cccb883e52e308fd7d0d87a8f59a7
SpyDev14 <135313844+SpyDev14@users.noreply.github.com>
2025-10-06 09:29:41 +0400

    rename Page url_path_name to just url_name

--- core/models/general.py
+++ core/models/general.py
@@ -38,11 +38,11 @@ class Page(BaseRenderableModel):
 			'<br>'
 			'Влияет на логику работы полей <code>URL Pathname</code> и <code>Template name</code>.'))
 
-	url_path_name = models.CharField("URL Pathname", max_length = 64, blank = True,
+	url_name = models.CharField("URL Name", max_length = 64, blank = True,
 		help_text = mark_safe(
 			'<code>is_generic_page:</code>✅ - Игнорируется, потому <b>должно быть</b> пустым.'
 			'<br>'
-			'<code>is_generic_page:</code>❌ - <code>name</code> того url path, который обрабатывает эту страницу.'
+			'<code>is_generic_page:</code>❌ - <code>name</code> того url, который обрабатывает эту страницу.'
 			'<details style="padding-left: 1rem;">'
 				'Например, в ваших <code>urlpatterns</code> есть <code>path("fun-path/", some_view, '
 				'name="your-name")</code>, тут указываете <code>"your-name"</code>.<br>'
@@ -50,7 +50,7 @@ class Page(BaseRenderableModel):
 				'path, т.е <code>"/fun-path/"</code>.<br>'
 				'<br>'
 				'Для index страницы по полному пути "/", а также для generic страниц с кастомным '
-				'URL в принципе, увы, но вы вынуждены создавать свои path и view, даже если не '
+				'URL в принципе, увы, но вы вынуждены создавать свои собственные url и view, даже если вам не '
 				'нужна дополнительная логика.' # В прочем, как-будто бы кому-то не пофиг, раньше
 				# все всегда делали View-ы под каждую страницу
 			'</details>'))
@@ -81,16 +81,16 @@ class Page(BaseRenderableModel):
 			})
 
 		# Проверка url_source
-		if self.is_generic_page and self.url_path_name:
+		if self.is_generic_page and self.url_name:
 			raise ValidationError({
-				'url_path_name': 'Должно быть пустым при is_generic_page:❌, так как '
+				'url_name': 'Должно быть пустым при is_generic_page:❌, так как '
 				'игнорируется (строгое обеспечение явности).'
 			})
 
 		if not self.is_generic_page:
 			try: self.get_absolute_url()
 			except NoReverseMatch: raise ValidationError({
-				'url_path_name': 'path с таким name не существует!'
+				'url_name': 'path с таким name не существует!'
 			})
 
 	def get_absolute_url(self):
@@ -98,7 +98,7 @@ class Page(BaseRenderableModel):
 			# Этот формат должен оставаться неизменным
 			return f'/{self.slug}/'
 
-		return reverse(self.url_path_name)
+		return reverse(self.url_name)
 
 	# В контексте объект Page живёт только 1 запрос,
 	# но внутри могут много раз обращаться к этому св-ву
